ca6e28f6124195b42c308b55f910ccdbc1e9819f
kellystickell <kstickell@deloitte.com>
2023-09-25 23:15:12 -0400

    Mobile navigation menu

---
+++ js/main.js
@@ -0,0 +1,4 @@
+let viewMoreBtn = document.querySelector('.readMore');
+let additionalContent = document.querySelector('.hidden')
+console.log('viewMore', viewMoreBtn)
+console.log('viewMore', additionalContent)
